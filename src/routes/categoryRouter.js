7e8d45b895cb6a940d8d09c35ad3e5f2a8a35c1b
Thanh Binh <thanhbinhkma27@gmail.com>
2022-06-21 22:06:17 +0700

    Update Category

--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -3,6 +3,9 @@ const express = require('express')
 
 const categoryRouter = express.Router()
 
+categoryRouter.get('/', categoryController.get)
 categoryRouter.post('/store', categoryController.store)
+categoryRouter.put('/:id', categoryController.update)
+categoryRouter.delete('/:id', categoryController.delete)
 
 module.exports = categoryRouter
