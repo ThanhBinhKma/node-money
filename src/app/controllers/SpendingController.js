d50bf5c03195c1be5135c5168753733055267c26
binh binh <binhtnt@kaopiz.com>
2022-06-22 09:06:19 +0700

    Add Spending

---
+++ src/app/controllers/SpendingController.js
@@ -0,0 +1,11 @@
+const Spending = require('../models/Spending')
+
+class SpendingController {
+    index(req, res) {}
+
+    store(req, res) {}
+
+    delete(req, res) {}
+}
+
+module.exports = new SpendingController()
---
+++ src/app/models/Spending.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const SpendingSchema = mongoose.Schema({
+    money: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now(),
+    },
+    category_id: {
+        type: String,
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now(),
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now(),
+    },
+})
+
+module.exports = mongoose.model('Spending', SpendingSchema)
